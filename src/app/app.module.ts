51fc26dd3202418845f81f21d8f9b4a97150b56b
unknown <aniket@greentinsolutions.com>
2024-04-11 12:13:39 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { MyJobsComponent } from './pages/my-jobs/my-jobs.component';
+import { JobListingComponent } from './pages/job-listing/job-listing.component';
+import { JobDetailsComponent } from './pages/job-details/job-details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateNewJobComponent } from './pages/create-new-job/create-new-job.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    LoginComponent,
+    RegistrationComponent,
+    JobsComponent,
+    MyJobsComponent,
+    JobListingComponent,
+    JobDetailsComponent,
+    HomeComponent,
+    CreateNewJobComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
